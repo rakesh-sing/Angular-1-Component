ce61a87ac79fd6012bf1af183c86cb89f93b8399
Rakesh Sing <rakesh-sing@outlook.com>
2025-04-27 01:39:54 +0530

    Property binding with disabled property

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styleUrl: './servers.component.css',
 })
 export class ServersComponent {
+  allowNewServer: boolean = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
