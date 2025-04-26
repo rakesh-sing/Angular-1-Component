6b19d4964c5561a2213593e9099a6c69c921b435
Rakesh Sing <rakesh-sing@outlook.com>
2025-04-27 02:04:30 +0530

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer: boolean = false;
-
+  serverName: string = 'Maza ma';
+  serverCreationStatus: string = 'No Server Available';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onServerCreation() {
+    return (this.serverCreationStatus = 'New Server is Created');
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event.target.value);
+    return (this.serverName = event.target.value);
   }
 }
