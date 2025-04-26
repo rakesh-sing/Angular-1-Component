978a1d7d9a7eb34e4605892cbac3b20eb3353f20
Rakesh Sing <rakesh-sing@outlook.com>
2025-04-27 01:30:59 +0530

    string Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrl: './server.component.css'
+  styleUrl: './server.component.css',
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'Online';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
